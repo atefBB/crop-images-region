f2a76e2668d23a04395452be162ce2d8a7749ff8
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-12 12:54:28 +0200

    docs: add comments

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const sharp = require("sharp");
 
+// put whatever folders names u wanna !
+const INPUT_FOLDER_NAME = "";
+const OUTPUT_FOLDER_NAME = "";
+
+// put whatever u wanna
 const PAIR_LEFT = 155;
 const PAIR_TOP = 95;
 const PAIR_WIDTH = 596;
 const PAIR_HEIGHT = 917;
 
+// put whatever u wanna
 const IMPAIR_LEFT = 78;
 const IMPAIR_TOP = 95;
 const IMPAIR_WIDTH = 596;
@@ -15,14 +21,14 @@ const TOTAL_PAGES_NUMBER = 606;
 
 for (let i = START_PAGE_NUMBER; i <= TOTAL_PAGES_NUMBER; i++) {
   if (i % 2 === 0) {
-    sharp(`./imgs/${i}.png`)
+    sharp(`./${INPUT_FOLDER_NAME}/${i}.png`)
       .extract({
         left: PAIR_LEFT,
         top: PAIR_TOP,
         width: PAIR_WIDTH,
         height: PAIR_HEIGHT,
       })
-      .toFile(`./warsh-asbahani_test/${i - 2}.png`, function (err) {
+      .toFile(`./${OUTPUT_FOLDER_NAME}/${i - 2}.png`, function (err) {
         if (err) console.log(err);
       });
   } else {
